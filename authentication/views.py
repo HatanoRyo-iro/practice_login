0f0f7707b7f0a38ab8de8113a8045b3c7b2604ff
RyoHatano <ryohatano0809@gmail.com>
2023-05-16 17:42:45 +0900

    add Implementation of activation after Email verification

--- authentication/views.py
+++ authentication/views.py
@@ -9,7 +9,7 @@ from django.core.mail import send_mail, EmailMessage
 
 from django.template.loader import render_to_string
 
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_text
 
 from login_function.info import EMAIL_HOST_USER_ADDRESS
@@ -73,12 +73,12 @@ def signup(request):
         
         current_sute = get_current_site(request)
         email_subject = 'Confirm your email @ GFG - Django Login!!'
-        message2 = render_to_string('email_confirmation.html'), {
+        message2 = render_to_string('email_confirmation.html', {
             'name' : myuser.first_name,
             'domain' : current_sute.domain,
             'uid' : urlsafe_base64_encode(force_bytes(myuser.pk)),
             'token' : generate_token.make_token(myuser),
-        }
+        })
         email = EmailMessage(
             email_subject,
             message2,
@@ -120,3 +120,18 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged Out Successfully!")
     return redirect('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+        
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
